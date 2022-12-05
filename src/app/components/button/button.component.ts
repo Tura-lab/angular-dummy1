f73a8fba674aad5b93230f8004e61c6556f9c892
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2022-12-05 21:09:05 +0300

    2 files modified

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,13 +10,16 @@ export class ButtonComponent implements OnInit {
   @Input() text: string
   @Input() textColor: string
   @Input() backgroundColor: string
+  @Output() btnClick = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {}
 
   onClick():void {
+    this.btnClick.emit();
     console.log("I just got clicked !")
+
   }
 
 }
